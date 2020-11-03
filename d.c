b92275a073f50a6dce81cdb702c352381cdc0b0a
Sheraz <sheraztariq147@gmail.com>
2020-11-03 19:20:18 +0100

    some changes in d.c

--- d.c
+++ d.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
+#include <string.h>
+
 
 int main(){
 char gender[20];
 printf("Please enter your gender :");
 scanf("%s",gender);
+
+
 char father;
 char mother;
-if ("gender[20] = father")
+if (!strcmp(gender,"father"))
 {
     printf("the gender is mother\n");
 }
-if ("gender[20] = mother")
+if (!strcmp(gender, "mother"))
 {
    printf("the gender is father\n");
 }
