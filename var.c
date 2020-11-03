baa725716a0d4908d6ecf5920b8f2a27e1504bb3
Sheraz <sheraztariq147@gmail.com>
2020-11-03 19:09:20 +0100

    sheraz's commit

--- var.c
+++ var.c
@@ -2,6 +2,13 @@
 
 int main(){
 
+
+
+
+
+
+
+	// some changes from sheraz
     // int i,j; // declaration 
     // i=100; // initialization
     char a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z; 
